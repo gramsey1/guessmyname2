26552beb0794b40a46c48f8d3ca45278241b6748
gramsey1 <graceramsey312@yahoo.com>
2018-10-22 11:20:52 -0400

    hola folks

---
+++ src/com/company/Main.java
@@ -0,0 +1,58 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	String[] name = new String [50];
+        name[1]= "Val";
+        name[2]= "Eden";
+        name[3]="Alex";
+        name[4]= "Bennett";
+        name[5]="Prem";
+        name[6]="Bryce";
+        name[7]="Sydney";
+        name[8]="Bobby";
+        name[9]="Emma";
+        name[10]="Tanner";
+        name[11]="Phillipe";
+        name[12]="Winston";
+        name[13]="Todd";
+        name[14]="Sean";
+        name[15]="Jacob";
+        name[16]="Bryan";
+        name[17]="Grace";
+        name[18]="Angelo";
+        name[19]="Amanda";
+        name[20]="Ruby";
+        name[21]="Eve";
+        name[22]="Audrey";
+        name[23]="Luke";
+        name[24]="Amy";
+        name[25]="Andy";
+        name[26]="Roman";
+        name[27]="Sydney";
+        name[28]="Ally";
+        name[29]="Derek";
+        name[30]="Carol";
+        name[31]="Buck";
+        name[32]="Jayson";
+        name[33]="Lyn";
+        name[34]="Chris";
+        name[35]="Michael";
+        name[36]="Lauren";
+        name[37]="Sofia";
+        name[38]="Lily";
+        name[39]="Lucio";
+        name[40]="Tommy";
+        name[41]="Nina";
+        name[42]="Matthew";
+        name[43]="Ian";
+        name[44]="Sue";
+        name[45]="Delaney";
+        name[46]="Hailey";
+        name[47]="Kathleen";
+        name[48]="Don";
+        name[49]="Jen";
+        name[50]="Mike";
+    }
+}
